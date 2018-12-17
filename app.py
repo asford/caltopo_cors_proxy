acf785270dff500d454b469948074f4e7229762f
Alex Ford <fordas@uw.edu>
2018-12-17 07:10:05 +0000

    Add initial index, kml render and proxy service.
    Add index.html, a material design lite demo adaptation w/ some wacky
    dynamic markdown rendering. Provides a workflow overview and quick
    button to extract map and Provides a workflow overview and quick button
    to extract map id and render proxy network link.
    
    Add network link kml rendering and cors-compatible map fetch proxy
    endpoint.

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, make_response, request
+from flask_cors import cross_origin
+
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/link/<map_id>")
+def network_link(map_id=None):
+
+    response = make_response(
+        render_template(
+            "network_link.kml", map_id=map_id, host=f"http://{request.headers['host']}"
+        )
+    )
+
+    response.headers["Content-Type"] = "application/vnd.google-earth.kml+xml"
+    response.headers["Content-Disposition"] = f"attachment; filename={map_id}.kml"
+
+    return response
+
+
+@app.route("/m/<map_id>")
+@cross_origin()
+def proxy_request(map_id=None):
+
+    proxy_request = requests.get(f"https://caltopo.com/m/{map_id}?format=kml")
+
+    response = make_response(proxy_request.text, proxy_request.status_code)
+
+    response.headers["Content-Type"] = proxy_request.headers["Content-Type"]
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
